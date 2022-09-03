ea81b61a76ab20a5f69766755134c145c2623a47
Parsa Frahani <parsa.farahani84@gmail.com>
2022-09-03 13:14:31 +0430

    finished the main section

--- parsataskmanager/src/components/Main/Main.jsx
+++ parsataskmanager/src/components/Main/Main.jsx
@@ -1,8 +1,93 @@
 import React from "react";
 import "./Main.css";
+import "boxicons";
+import img from "../../img/p.svg";
+import img2 from "../../img/p1.svg";
+import img3 from "../../img/p2.svg";
+import img4 from "../../img/p3.svg";
+import wave1 from "../../img/wave1.svg";
+import wave2 from "../../img/wave2.svg";
+import wave3 from "../../img/wave3.svg";
+import wave4 from "../../img/wave4.svg";
+import logo from "../../img/PTM2.png";
 
 function Main() {
-  return <div>Main</div>;
+  const data = [
+    {
+      title: "Today",
+      icon: `${img}`,
+      border: "4px solid #eb1d36",
+      wave: `${wave1}`,
+    },
+    {
+      title: "This Week",
+      icon: `${img2}`,
+      border: "4px solid #3B9AE1",
+      wave: `${wave2}`,
+    },
+    {
+      title: "This Month",
+      icon: `${img3}`,
+      border: "4px solid #E38B29",
+      wave: `${wave3}`,
+    },
+
+    {
+      title: "Preorities",
+      icon: `${img4}`,
+      border: "4px solid #645CAA",
+      wave: `${wave4}`,
+    },
+  ];
+
+  return (
+    <div className="main-container">
+      <div className="main-tasks">
+        {data.map((e) => (
+          <div className="card" style={{ border: `${e.border}` }}>
+            <div className="flex">
+              <h2>{e.title}</h2>
+              <img className="icon" src={e.icon} />
+            </div>
+            <button className="mbtn">Let's see</button>
+            <img src={`${e.wave}`} className="wave" />
+          </div>
+        ))}
+      </div>
+      <div className="rutins"></div>
+
+      <div className="menu">
+        <div className="header-m">
+          <img src={logo} className="m-logo" />
+          <h3>MENU</h3>
+        </div>
+
+        <ul className="items">
+          <li className="item">
+            Today<box-icon type="solid" name="calendar-event"></box-icon>
+          </li>
+          <li className="item">
+            This Week<box-icon name="calendar" type="solid"></box-icon>
+          </li>
+          <li className="item">
+            This Month<box-icon type="solid" name="calendar-week"></box-icon>
+          </li>
+          <li className="item">
+            Contact Us<box-icon name="phone" type="solid"></box-icon>
+          </li>
+          <li className="item">
+            Info<box-icon name="info-circle" type="solid"></box-icon>
+          </li>
+        </ul>
+        <div className="social">
+          <box-icon name="instagram" type="logo" className="th"></box-icon>
+          <box-icon type="logo" name="twitter"></box-icon>{" "}
+          <box-icon name="telegram" type="logo"></box-icon>{" "}
+          <box-icon name="facebook-circle" type="logo"></box-icon>{" "}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Main;
